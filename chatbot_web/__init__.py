ef32afa707fb421010124779034eeae6172233ea
MiaTian1024 <sddxtm123@gmail.com>
2023-05-08 18:50:36 +1200

    backend working

--- chatbot_web/__init__.py
+++ chatbot_web/__init__.py
@@ -3,4 +3,5 @@ from flask import Flask
 app=Flask(__name__)
 
 from chatbot_web import routes
+from chatbot_web import chatbot
 
--- chatbot_web/routes.py
+++ chatbot_web/routes.py
@@ -4,8 +4,22 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+import sys
+from chatbot_web import chatbot
+import threading
+import os
 
 
-@app.route("/")
+@app.route("/",methods=["GET", "POST"])
 def home():
+    dashboard=[]
+    if request.method == 'POST':
+        question = request.form.get('question')
+        dashboard.append(question)
+        print(question)
+        answer = chatbot.get_response(question)
+        dashboard.append(answer)
+        print(answer)
+        print(dashboard)
+        return render_template("homepage.html", dashboard=dashboard, question=question, answer=answer)
     return render_template("homepage.html")
